d0f66f39d44754c89ca32d15160a6e9dbb044664
niyatimadaan <madaanniyati@gmail.com>
2024-02-23 09:36:07 +0530

    code refactoring

--- src/app.js
+++ src/app.js
@@ -3,30 +3,13 @@ import authRoutes from './routes/authRoutes.js';
 import communityRoutes from './routes/communityRoutes.js';
 import roleRoutes from './routes/roleRoutes.js';
 import memberRoutes from './routes/memberRoutes.js';
-import swaggerJSDoc from 'swagger-jsdoc';
-import { setup } from 'swagger-ui-express';
-import { serve } from 'swagger-ui-express';
 import bodyParser from 'body-parser';
 import swaggerDocs from './utils/swagger.js';
-// import {swaggerDocument} from '../swagger.json' 
-
-// const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 
 app.use(bodyParser.json());
-// app.use('/docs', serve, setup(swaggerDocs));
-/**
-   * @openapi
-   * /v1/auth:
-   *  get:
-   *     tags:
-   *     - Healthcheck
-   *     description: Responds if the app is up and running
-   *     responses:
-   *       200:
-   *         description: App is up and running
-   */
+
 app.use('/v1/auth', authRoutes);
 app.use('/v1/community', communityRoutes);
 app.use('/v1/role', roleRoutes);
@@ -38,11 +21,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   swaggerDocs(app);
 });
-
-
-// const swaggerDocument = require('./swagger.json');
-// const customCss = fs.readFileSync((process.cwd()+"/swagger.css"), 'utf8');
-// let express to use this
-// app.use(bodyParser.json());
-// app.use('/api-docs', serve, setup());
-  // swaggerDocument, {customCss}));
